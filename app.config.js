b70c4629898f48db45b312a3645f6cbcb6b9207b
Alstudd <alstonsoares17@gmail.com>
2024-10-09 10:21:03 +0530

    eas

--- app.config.js
+++ app.config.js
@@ -1,56 +1,53 @@
 export default {
-    "expo": {
-      "name": "AntiMatrix",
-      "slug": "radar_paypal_mafia",
-      "version": "1.0.0",
-      "orientation": "portrait",
-      "icon": "./assets/images/AntiMatrix.png",
-      "scheme": "antimatrix",
-      "userInterfaceStyle": "automatic",
-      "splash": {
-        "image": "./assets/images/AntiMatrix.png",
-        "resizeMode": "cover",
-        "backgroundColor": "#ffffff"
+  expo: {
+    name: "AntiMatrix",
+    slug: "radar_paypal_mafia",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/AntiMatrix.png",
+    scheme: "antimatrix",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/AntiMatrix.png",
+      resizeMode: "cover",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "radar-paypal-mafia",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/AntiMatrix.png",
+        backgroundColor: "#ffffff",
       },
-      "ios": {
-        "supportsTablet": true,
-        "bundleIdentifier": "radar-paypal-mafia"
-      },
-      "android": {
-        "adaptiveIcon": {
-          "foregroundImage": "./assets/images/AntiMatrix.png",
-          "backgroundColor": "#ffffff"
+      package: "com.alstudd.radar_paypal_mafia",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    web: {
+      bundler: "metro",
+      output: "server",
+      favicon: "./assets/images/AntiMatrix.png",
+    },
+    plugins: [
+      [
+        "expo-router",
+        {
+          origin: "https://radar_paypal_mafia.dev/",
         },
-        "package": "com.alstudd.radar_paypal_mafia",
-        "googleServicesFile": process.env.GOOGLE_SERVICES_JSON
-      },
-      "web": {
-        "bundler": "metro",
-        "output": "server",
-        "favicon": "./assets/images/AntiMatrix.png"
-      },
-      "plugins": [
-        [
-          "expo-router",
-          {
-            "origin": "https://radar_paypal_mafia.dev/"
-          }
-        ],
-        [
-          "@react-native-google-signin/google-signin"
-        ]
       ],
-      "experiments": {
-        "typedRoutes": true
+      ["@react-native-google-signin/google-signin"],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: "https://radar_paypal_mafia.dev/",
       },
-      "extra": {
-        "router": {
-          "origin": "https://radar_paypal_mafia.dev/"
-        },
-        "eas": {
-          "projectId": "3cbe5c9c-8ec0-4dbc-a2dc-54788982f469"
-        }
-      }
-    }
-  }
-  
+      eas: {
+        projectId: "abf90122-58b8-49b8-9f11-3da269f6e4f8",
+      },
+    },
+  },
+};
